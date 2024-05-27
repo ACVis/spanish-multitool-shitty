b3128ac91a52812f66bd9355013616441c671914
Alex <17129355+ACVis@users.noreply.github.com>
2024-05-27 14:16:41 -0400

    chore: Update MongoDB connection options
    The MongoDB connection options in the code have been updated to remove the `useNewUrlParser` option and add the `useUnifiedTopology` option. This change ensures compatibility with the latest version of MongoDB.

--- runtst.js
+++ runtst.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 function test() {
   mongoose
-    .connect(`mongodb://localhost:27017`, {
-      useNewUrlParser: true,
-    })
+    .connect(`mongodb://localhost:27017`, {})
     .then(() => console.log("MongoDB connection established."))
     .catch((error) =>
       console.error("MongoDB connection failed:", error.message)
